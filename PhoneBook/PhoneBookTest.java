3084343c663cb40fefc7b18260bb0bea2632bebc
JunKiBeom <dona0408@gmail.com>
2020-05-08 12:58:31 +0900

    add sum func in PhoneBookManager

--- PhoneBook/PhoneBookTest.java
+++ PhoneBook/PhoneBookTest.java
@@ -10,7 +10,15 @@ class PhoneBook {
 }
 
 class PhoneBookManager {
+    public void insert(String i, String n, String num, String e) {  // 신규 회원 추가
 
+    }
+    public void delete(String i) {  // 관리번호로 삭제
+
+    }
+    public void modify(String n) { // 기존 회원의 전화번호 변경
+
+    }
 }
 
 public class PhoneBookTest {
