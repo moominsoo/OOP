0e6dfc41e940a1b46d822d674b7906eb248f6605
JunKiBeom <dona0408@gmail.com>
2020-05-10 13:56:35 +0900

    add class Player

---
+++ Marble/Marble.java
@@ -0,0 +1,21 @@
+package Marble;
+class Player {
+	int balance;
+	int loc;
+	Player(){
+		this.balance=5000;
+		this.loc=0;
+	};
+}
+
+class City {
+
+}
+
+public class Marble {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Player p1 = new Player();
+		Player p2 = new Player();
+	}
+}
